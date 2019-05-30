c8fb902ad215ccc46db001c9d9793d815c2616cd
frescoplaylab <38917706+frescoplaylab@users.noreply.github.com>
2019-05-30 13:04:43 +0530

    Commit maven-war project

---
+++ src/main/java/com/fresco/play/App.java
@@ -0,0 +1,13 @@
+package com.fresco.play;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Welcome to TCS</h2>
+</body>
+</html>
